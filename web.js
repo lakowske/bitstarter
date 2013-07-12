1af7b93c890a370ff9e99be2b206a850d146272b
Seth Lakowske <lakowske@gmail.com>
2013-07-12 00:51:10 +0000

    now read from index.html

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+pageFunc = function () {
+    return fs.readFileSync('index.html', 'utf8');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(pageFunc());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
